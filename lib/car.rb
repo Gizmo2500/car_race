3ff094081f78aff8946e7779a73461e9481ba87e
David Spencer <davidspencer@Davids-MacBook-Pro-2.local>
2015-04-20 16:54:26 -0700

    add car and race class

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,12 @@
 # Add Car class here
+class Car 
+	attr_accessor :speed
+
+	def initialize
+		@speed = 0
+	end
+
+	def accelerate(gain) 
+		@speed += gain
+	end
+end
--- lib/race.rb
+++ lib/race.rb
@@ -1,3 +1,33 @@
 require_relative 'car'
 
 # Add Race class here
+class Race
+    attr_accessor :racers
+
+    def initialize
+    	@racers = []
+    	start
+    end
+
+    def winner 
+		@racers.sort_by(&:speed).first
+    end
+
+    def loser
+    	@racers.sort_by(&:speed).last
+    end
+
+private
+
+	def random_speed
+		(rand * 100).round
+	end
+
+	def start
+		@racers << Car.new
+		@racers << Car.new
+
+		@cars.first.accelerate(random_speed)
+		@cars.last.accelerate(random_speed)
+	end
+end
